ffe4eef1a91f111ea27c7e9c3d9aa78946cd1944
RCNR <iop04226@gmail.com>
2025-10-19 23:58:52 +0900

    Feat: 로그인 설정 - 자체(커스텀) 로그인 필터 설정 - 그에 따른 securityFilterChain, AuthenticationManager 설정 - UserDetails

--- backend/src/main/java/org/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/config/SecurityConfig.java
@@ -1,20 +1,88 @@
 package org.example.backend.config;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.backend.filter.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
 
     // 암호화 진행하는 메서드
     // 비밀번호는 BCrypt로 단방향 암호화 Bean
     @Bean
     public PasswordEncoder passwordEncoder() { // 인터페이스 타입으로 리턴
         return new BCryptPasswordEncoder(); // 구현체 리턴
+    }
+
+    // 커스텀 로그인 필터를 위한 AuthenticationManager 빈 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+    // securityFilterChain 설정
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // stateless 하기에 csrf 비활성화
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // CORS 설정
+
+        // 기본 form 기반 로그인 비활성화 -> Login Filter 직접 작성 필요
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        // 기본 basic 인증 필터 비활성화
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        // 인가
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        //  커스터 필터 (LoginFilter) 추가
+        /**
+         * 여기서 UsernamePasswordAuthenticationFilter.class 는
+         * 기준이 되는 필터이다.
+         * LoginFilter는 생성자로부터 AUthenticationManager(인증 진행 인터페이스)를 주입 받는다.
+         * 따라서 직접 넣어야 한다.
+         */
+        http.addFilterBefore(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
+        // 예외 처리
+        http
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 로그인 X
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.sendError(HttpServletResponse.SC_FORBIDDEN); // 권한 X
+                        })
+                );
+
+        // 세션 필터 설정 - stateless 설정
+        http.sessionManagement(session -> session.
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
     }
 }
